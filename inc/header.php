cd36e9239625a6adfd24749c63650e4f7b86228e
Matt G <matt.gaikema@tamu.edu>
2016-04-10 18:46:02 -0500

    Dynamic planning

---
+++ inc/header.php
@@ -0,0 +1,23 @@
+<!Doctype html>
+<html>
+<head>
+	<title>Matt Gaikema</title>
+	<meta name='author' content='Matt Gaikema'></meta>
+	<meta name='description' content='My website'></meta>
+	<meta name='keywords' content='Matt,Gaikema,Math,Computers,competent,smart,hirable'></meta>
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<meta charset="UTF-8">
+	<link rel='icon' href='https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/5/005/092/2c5/35c85cc.jpg'></link>
+	<link rel='stylesheet' type='text/css' href='styles/style.css'></link>
+	<link rel='stylesheet' href='http://bootswatch.com/superhero/bootstrap.css'></link>
+	<script>
+	  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+	  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+	  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+	  })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+
+	  ga('create', 'UA-73392139-1', 'auto');
+	  ga('send', 'pageview');
+
+	</script>	
+</head>
