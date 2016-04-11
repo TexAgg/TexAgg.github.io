f26d26dc5aad9098ff89a81987a3bec94a486856
Matt G <matt.gaikema@tamu.edu>
2016-04-10 19:34:54 -0500

    Added projects page

--- index.php
+++ index.php
@@ -8,6 +8,7 @@
 		<!--<h1 class="branding-title"><a href="./">Matt Gaikema</a></h1>-->
 		<ul class="nav">
    			<li class="about"<?php if ($section=="about"){echo " on";} ?>"><a href="about.php">About me</a></li>
+			<li class="projects"<?php if ($section=="projects"){echo " on";} ?>"><a href="projects.php">Projects</a></li>
 		</ul>
 	</div>
 </div>
