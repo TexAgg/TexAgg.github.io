9ab40d1dc8e83efef70d0e561dab390548a1121d
Matt G <matt.gaikema@tamu.edu>
2016-05-21 21:50:31 -0500

    Edit style

--- inc/header.php
+++ inc/header.php
@@ -1,4 +1,4 @@
-<html>
+<html lang="en">
 <head>
 	<title><?php echo($pageTitle);?></title>
 	<meta name='author' content='Matt Gaikema'></meta>
@@ -7,8 +7,18 @@
 	<meta name="viewport" content="width=device-width, initial-scale=1">
 	<meta charset="UTF-8">
 	<link rel='icon' href='https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/5/005/092/2c5/35c85cc.jpg'></link>
-	<link rel='stylesheet' type='text/css' href='styles/bootstrap.min.css'></link>
+	<!--<link rel='stylesheet' type='text/css' href='styles/bootstrap.min.css'></link>-->
 	<link rel='stylesheet' type='text/css' href='styles/style.css'></link>
+	
+	<script src="https://ajax.googleapis.com/ajax/libs/jquery/2.2.3/jquery.min.js"></script>
+	
+	<!-- Latest compiled and minified CSS -->
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" integrity="sha384-1q8mTJOASx8j1Au+a5WDVnPi2lkFfwwEAa8hDDdjZlpLegxhjVME1fgjWPGmkzs7" crossorigin="anonymous">
+	<!-- Optional theme -->
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap-theme.min.css" integrity="sha384-fLW2N01lMqjakBkx3l/M9EahuwpSfeNvV63J5ezn3uZzapT0u7EYsXMjQV+0En5r" crossorigin="anonymous">
+	<!-- Latest compiled and minified JavaScript -->
+	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js" integrity="sha384-0mSbJDEHialfmuBBQP6A4Qrprq5OVfW37PRR3j5ELqxss1yVqOtnepnHVP9aJ7xS" crossorigin="anonymous"></script>
+	
 	<script>
 	  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
 	  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
@@ -20,6 +30,7 @@
 
 	</script>	
 </head>
+
 <body>
 	
 	<header>
--- index.php
+++ index.php
@@ -3,9 +3,8 @@
 	$section = null;
 	include("inc/header.php");
 ?>
-<div class="header">
+<!--<div class="header">
 	<div class="wrapper">
-		<!--<h1 class="branding-title"><a href="./">Matt Gaikema</a></h1>-->
 		<ul class="nav">
    			<li class="about"<?php if ($section=="about"){echo " on";} ?>"><a href="about.php">About me</a></li>
 			<li class="projects"<?php if ($section=="projects"){echo " on";} ?>"><a href="projects.php">Projects</a></li>
@@ -13,7 +12,13 @@
 			<li class="resume"<?php if ($section=="resume"){echo " on";} ?>"><a href="resume.php">Resume</a></li>
 		</ul>
 	</div>
-</div>
+</div>-->
+<ul class="nav nav-tabs">
+  <li role="presentation" class="about"<?php if ($section=="about"){echo " on";} ?>"><a href="about.php">About me</a></li>
+  <li role="presentation" class="projects"<?php if ($section=="projects"){echo " on";} ?>"><a href="projects.php">Projects</a></li>
+  <li role="presentation" class="skills"<?php if ($section=="skills"){echo " on";} ?>"><a href="skills.php">Skills</a></li>
+  <li role="presentation" class="resume"<?php if ($section=="resume"){echo " on";} ?>"><a href="resume.php">Resume</a></li>
+</ul>
 <?php
 	include("inc/footer.php");
 ?>	
