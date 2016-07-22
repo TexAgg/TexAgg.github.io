a7050e008f9b53f76c42a579cc7271eb61b85d64
Matt G <matt.gaikema@tamu.edu>
2016-07-22 11:47:47 -0500

    Stop previous clippy animation before next one

--- src/js/main.js
+++ src/js/main.js
@@ -8,23 +8,28 @@ clippy.load('Clippy', function(agent) {
 	*/
 
 	document.getElementById('index-nav-li').onmouseenter = function() {
+		agent.stopCurrent();
 		agent.speak("Click here to read about me!");
 	}
 
 	document.getElementById('projects-nav-li').onmouseenter = function() {
+		agent.stopCurrent();
 		agent.speak("Click here to see some projects I have worked on!");
 	}
 
 	document.getElementById('skills-nav-li').onmouseenter = function() {
+		agent.stopCurrent();
 		agent.speak("Click here to see my skills!");
 	}
 
 	document.getElementById('resume-nav-li').onmouseenter = function() {
+		agent.stopCurrent();
 		agent.speak("Click here to see my resume!");
 	}
 
 	document.getElementById('contact-nav-li').onmouseenter = function() {
+		agent.stopCurrent();
 		agent.speak('Click here to see my contact information!');
 	}
-	
+
 });
